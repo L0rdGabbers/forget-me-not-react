89ad867fd27b09c74558259edfa4dc0453bffe1a
L0rdGabbers <gabrielmarriage@me.com>
2023-12-12 18:16:33 +0000

    Adds dropdown menu for friends related pages

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,49 @@
 import React from 'react';
 import styles from '../styles/NavBar.module.css'
-import { Navbar, Nav } from 'react-bootstrap';
-import { NavLink } from 'react-router-dom/cjs/react-router-dom.min';
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import { NavLink, useLocation } from 'react-router-dom';
 import { useCurrentUser } from '../contexts/CurrentUserContext';
 
 const NavBar = () => {
   const currentUser = useCurrentUser();
-  const loggedInIcons = <>{currentUser?.username}</>;
+  const location = useLocation();
+
+  const getCurrentDate = () => {
+    const currentDate = new Date();
+    const options = { day: 'numeric', month: 'short', year: 'numeric' };
+    return currentDate.toLocaleDateString('en-UK', options);
+  };
+
+  const isFriendsActive = ['/friends/send-request', '/friends/view-list', '/friends/view-requests'].some(
+    (path) => location.pathname.startsWith(path)
+  );
+
+  const addPostIcon = (
+    <>
+      <NavLink
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+        to="/projects/create"
+      >
+        <i className="fa-solid fa-file-circle-plus"></i> Create Project
+      </NavLink>
+    </>
+  );
+  const loggedInIcons = (
+    <>
+      <NavDropdown title={<i className={`fas fa-user-friends ${isFriendsActive ? styles.Active : ''}`}></i>} id="basic-nav-dropdown">
+        <NavDropdown.Item as={NavLink} to="/friends/send-request">
+          Add Friend
+        </NavDropdown.Item>
+        <NavDropdown.Item as={NavLink} to="/friends/view-list">
+          Friend List
+        </NavDropdown.Item>
+        <NavDropdown.Item as={NavLink} to="/friends/view-requests">
+          Friend Requests
+        </NavDropdown.Item>
+      </NavDropdown>
+    </>
+  );
   const loggedOutIcons = (
     <>
       <NavLink
@@ -31,6 +68,8 @@ const NavBar = () => {
       <NavLink to="/">
         <Navbar.Brand className={styles.Title}>Forget Me Not</Navbar.Brand>
       </NavLink>
+      <span>{getCurrentDate()}</span>
+      {currentUser && addPostIcon}
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="ml-auto">
